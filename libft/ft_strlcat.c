e855f97080897bb29537b88aa418790a28db272d
zhy2on <dhwlgus5940@gmail.com>
2021-05-20 17:09:19 +0900

    edit strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,30 +6,27 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 17:40:33 by jihoh             #+#    #+#             */
-/*   Updated: 2021/05/20 15:18:13 by jihoh            ###   ########.fr       */
+/*   Updated: 2021/05/20 17:07:22 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	i;
-	size_t	j;
+	size_t i;
+	size_t j;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
 	i = 0;
-	while (i < dstsize && dst[i])
-		i++;
 	j = 0;
-	while (j < src_len && dst_len + j + 1 < dstsize)
+	while (dst[i] && i < dstsize)
+		i++;
+	while (src[j] && (i + j + 1) < dstsize)
 	{
-		dst[dst_len + j] = src[j];
+		dst[i + j] = src[j];
 		j++;
 	}
-	dst[dst_len + j] = '\0';
-	return (i + src_len);
+	if (i < dstsize)
+		dst[i + j] = '\0';
+	return (i + ft_strlen(src));
 }
