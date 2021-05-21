b83211a5cc1af2a4eff145cf75c8e55c1b1edad3
zhy2on <dhwlgus5940@gmail.com>
2021-05-21 19:14:51 +0900

    PART2

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,86 +6,79 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 17:35:33 by jihoh             #+#    #+#             */
-/*   Updated: 2021/05/21 17:37:57 by jihoh            ###   ########.fr       */
+/*   Updated: 2021/05/21 17:45:07 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char			**ft_freeall(char **s)
+char			**free_strs(char **strs)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (s[i] != NULL)
+	while (*strs)
 	{
-		free(s[i]);
-		i++;
+		free(*strs);
+		strs++;
 	}
-	free(s);
+	free(strs);
 	return (NULL);
 }
 
-size_t			ft_wordcnt(const char *s, char c)
+size_t			strs_size(const char *s, char c)
 {
-	size_t			cnt;
-	size_t			i;
+	size_t			size;
 
-	cnt = 0;
-	i = 0;
-	while (s[i])
+	size = 0;
+	while (*s)
 	{
-		if (s[i] != c)
+		if (*s != c)
 		{
-			cnt++;
-			while (s[i] && s[i] != c)
-				i++;
+			size++;
+			while (*s && *s != c)
+				s++;
 		}
 		else
-			i++;
+			s++;
 	}
-	return (cnt);
+	return (size);
 }
 
-char			*ft_worddup(const char *s, char c)
+char			*worddup(const char *s, char c)
 {
 	size_t			len;
-	char			*ptr;
+	char			*ret;
 
 	len = 0;
 	while (s[len] && s[len] != c)
 		len++;
-	if (!(ptr = (char *)ft_calloc(len + 1, sizeof(char))))
+	if (!(ret = (char *)ft_calloc(len + 1, sizeof(char))))
 		return (NULL);
-	ft_strlcpy(ptr, s, len + 1);
-	return (ptr);
+	ft_strlcpy(ret, s, len + 1);
+	return (ret);
 }
 
 char			**ft_split(const char *s, char c)
 {
-	char			**ptr;
-	size_t			len;
+	char			**strs;
+	size_t			size;
 	size_t			i;
-	size_t			j;
 
 	if (!s)
 		return (NULL);
-	len = ft_wordcnt(s, c);
-	if (!(ptr = (char **)ft_calloc(len + 1, sizeof(char *))))
+	size = strs_size(s, c);
+	if (!(strs = (char **)ft_calloc(size + 1, sizeof(char *))))
 		return (NULL);
 	i = 0;
-	j = 0;
-	while (i < len && s[j])
+	while (i < size && *s)
 	{
-		if (s[j] != c)
+		if (*s != c)
 		{
-			if (!(ptr[i++] = ft_worddup(&(s[j]), c)))
-				return (ft_freeall(ptr));
-			while (s[j] && s[j] != c)
-				j++;
+			if (!(strs[i++] = worddup(s, c)))
+				return (free_strs(strs));
+			while (*s && *s != c)
+				s++;
 		}
 		else
-			j++;
+			s++;
 	}
-	return (ptr);
+	return (strs);
 }
