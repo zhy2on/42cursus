caef1ad00cacfdd06b58f95fbf9e2a0de713f093
zhy2on <dhwlgus5940@gmail.com>
2021-08-18 19:11:22 +0900

    update putnbr

--- 00_Libft/srcs/ft_putnbr_fd.c
+++ 00_Libft/srcs/ft_putnbr_fd.c
@@ -14,18 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
+	unsigned int	nb;
+
+	nb = n;
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-n, fd);
+		nb *= -1;
 	}
-	else if (n >= 10)
+	if (nb >= 10)
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(n % 10 + '0', fd);
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putchar_fd(nb % 10 + '0', fd);
 	}
 	else
-		ft_putchar_fd(n + '0', fd);
+		ft_putchar_fd(nb + '0', fd);
 }
